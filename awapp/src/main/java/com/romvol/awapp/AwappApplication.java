12ecf13e6f6ec3f6873a86d4497541cd61891adb
ar17 <archtector@gmail.com>
2018-03-18 23:46:32 +0200

    Initial commit

---
+++ awapp/src/main/java/com/romvol/awapp/AwappApplication.java
@@ -0,0 +1,12 @@
+package com.romvol.awapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwappApplication.class, args);
+	}
+}

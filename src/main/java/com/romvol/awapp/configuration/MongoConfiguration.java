7a053d8d92598e90de8171dc4d2ababb6e761fd0
ar17 <archtector@gmail.com>
2018-03-22 21:02:08 +0200

    add MongoConfiguration

---
+++ src/main/java/com/romvol/awapp/configuration/MongoConfiguration.java
@@ -0,0 +1,40 @@
+package com.romvol.awapp.configuration;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@Configuration
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@EnableReactiveMongoRepositories(basePackages = "com.romvol.awapp.repository")
+public class MongoConfiguration extends AbstractReactiveMongoConfiguration {
+
+
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
+    @Override
+    protected String getDatabaseName() {
+        return databaseName;
+    }
+
+    @Override
+//    @Bean
+    public MongoClient reactiveMongoClient() {
+        return MongoClients.create();
+    }
+
+
+//    @Bean
+//    public ReactiveMongoTemplate reactiveMongoTemplate() {
+//        return new ReactiveMongoTemplate(reactiveMongoClient(), getDatabaseName());
+//    }
+}

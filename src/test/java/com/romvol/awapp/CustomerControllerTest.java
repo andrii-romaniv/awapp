87c9f517f3312c78e5568b8f010d38609dfa929b
ar17 <archtector@gmail.com>
2018-04-03 22:37:07 +0300

    add update delete test

--- src/test/java/com/romvol/awapp/CustomerControllerTest.java
+++ src/test/java/com/romvol/awapp/CustomerControllerTest.java
@@ -10,8 +10,12 @@ import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
@@ -52,6 +56,95 @@ public class CustomerControllerTest {
 
     }
 
+    @Test
+    public void getCustomerByPhoneNumber() {
+        given(customerRepository.findByPhoneNumber("+380660000000"))
+                .willReturn(Mono.just(Customer.builder()
+                        .id("1")
+                        .firstName("User")
+                        .lastName("Test")
+                        .phoneNumber("+380660000000")
+                        .build()));
+
+        client.get().uri("/customers/+380660000000").exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.firstName").isEqualTo("User")
+                .jsonPath("$.lastName").isEqualTo("Test")
+                .jsonPath("$.id").isEqualTo("1")
+                .jsonPath("$.phoneNumber").isEqualTo("+380660000000");
+
+        verify(customerRepository, times(1)).findByPhoneNumber(anyString());
+        verifyNoMoreInteractions(customerRepository);
+    }
+
+    @Test
+    public void createCustomer() {
+        Customer customer = Customer.builder()
+                .firstName("User")
+                .lastName("Test")
+                .phoneNumber("+380660000000")
+                .build();
+
+        given(customerRepository.save(customer))
+                .willReturn(Mono.just(Customer.builder()
+                        .id("1")
+                        .firstName("User")
+                        .lastName("Test")
+                        .phoneNumber("+380660000000")
+                        .build()));
+
+        client.post().uri("/customers").body(BodyInserters.fromObject(customer))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.id").isEqualTo("1")
+                .jsonPath("$.firstName").isEqualTo("User")
+                .jsonPath("$.lastName").isEqualTo("Test")
+                .jsonPath("$.phoneNumber").isEqualTo("+380660000000");
+
+        verify(customerRepository, times(1)).save(any(Customer.class));
+        verifyNoMoreInteractions(customerRepository);
+    }
+    
+    @Test
+    public void updateCustomer() {
+        Customer customer = Customer.builder()
+                .id("1")
+                .firstName("User")
+                .lastName("Test")
+                .phoneNumber("+380660000000")
+                .build();
+
+        given(customerRepository.findById("1"))
+                .willReturn(Mono.just(customer));
+
+        customer.setDiscount(BigDecimal.valueOf(10.0));
+
+        given(customerRepository.save(customer))
+                .willReturn(Mono.just(
+                        Customer.builder()
+                                .id("1")
+                                .firstName("User")
+                                .lastName("Test")
+                                .phoneNumber("+380660000000")
+                                .discount(BigDecimal.valueOf(10.0))
+                                .build()));
+
+        client.put().uri("/customers/1").body(BodyInserters.fromObject(customer))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.firstName").isEqualTo("User")
+                .jsonPath("$.lastName").isEqualTo("Test")
+                .jsonPath("$.id").isEqualTo("1")
+                .jsonPath("$.discount").isEqualTo("10.0")
+                .jsonPath("$.phoneNumber").isEqualTo("+380660000000");
+
+        verify(customerRepository, times(1)).findById(anyString());
+        verify(customerRepository, times(1)).save(any(Customer.class));
+        verifyNoMoreInteractions(customerRepository);
+    }
 
     @Test
     public void deleteCustomer() {
@@ -76,5 +169,7 @@ public class CustomerControllerTest {
         verify(customerRepository, times(1)).delete(any(Customer.class));
         verifyNoMoreInteractions(customerRepository);
     }
+
+
 }
 

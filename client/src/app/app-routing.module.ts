508cba52deabc0161cc32f3c984845fbbebd5cbf
ar17 <archtector@gmail.com>
2018-04-26 20:28:32 +0300

    change app js and components, not sure it works

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoadGuard } from './core/load-guard';
+
+// import { AppRoutingComponent } from './name.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'user', loadChildren: './user/user.module#UserModule' },
+  {
+    path: 'auth',
+    loadChildren: './auth/auth.module#AuthModule',
+    canLoad: [LoadGuard]
+  }
+  // { path: '**', component:PageNotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
+
+// export const routedComponents = [AppRoutingComponent];
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { AuthService } from './core/auth.service';
+import { MatIconRegistry, MatDialog } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+import { DialogComponent } from './dialog/dialog.component';
+import 'rxjs/add/operator/filter';
+import { User } from './core/user.model';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Awesome App';
+export class AppComponent implements OnInit {
+
+  links = [
+    {
+      icon: 'home',
+      path: '',
+      label: 'HOME'
+    }
+  ];
+
+  isDarkTheme = false;
+  currentUser: Observable<User>;
+
+  constructor(
+    private auth: AuthService,
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer,
+    private dialog: MatDialog
+  ) {
+    // To avoid XSS attacks, the URL needs to be trusted from inside of your application.
+    const avatarsSafeUrl = this.sanitizer.bypassSecurityTrustResourceUrl('./assets/avatars.svg');
+    this.iconRegistry.addSvgIconSetInNamespace('avatars', avatarsSafeUrl);
+    this.currentUser = this.auth.currentUser();
+  }
+  ngOnInit(): void {
+    console.log('calling ngOnInit...');
+    this.auth.verifyAuth();
+  }
+
+  signout() {
+    this.auth.signout();
+  }
+
 }
